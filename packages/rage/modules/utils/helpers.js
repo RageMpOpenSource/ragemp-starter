7e837f5a6b1a907abef5992975c02eb5799a3eaa
Lucas Godoy <lcsdegodoy@outlook.com>
2018-12-17 00:28:34 -0200

    chore: remove leftover files

--- packages/rage/modules/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-exports.randomInt = randomInt;
